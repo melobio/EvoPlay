580d5410c246a4ad71fa3aed70603a970726f553
wangyi880907 <46990693+wangyi880907@users.noreply.github.com>
2023-08-23 17:22:11 +0800

    Add files via upload

---
+++ code/PAB1_GFP_task/evoplay_custom_design_with_preselected_sites/sequence_env_differ.py
@@ -0,0 +1,314 @@
+# -*- coding: utf-8 -*-
+"""
+An implementation of the training pipeline of EvoPlay for PAB1/GFP protein mutation
+
+@author: Yi Wang
+"""
+
+from __future__ import print_function
+import numpy as np
+import torch
+import random
+from typing import List, Union
+
+AAS = "ILVAGMFYWEDQNHCRKSTP"
+def string_to_one_hot(sequence: str, alphabet: str) -> np.ndarray:
+
+    out = np.zeros((len(sequence), len(alphabet)))
+    for i in range(len(sequence)):
+        out[i, alphabet.index(sequence[i])] = 1
+    return out
+
+def one_hot_to_string(
+    one_hot: Union[List[List[int]], np.ndarray], alphabet: str
+) -> str:
+    """
+    Return the sequence string representing a one-hot vector according to an alphabet.
+
+    Args:
+        one_hot: One-hot of shape `(len(sequence), len(alphabet)` representing
+            a sequence.
+        alphabet: Alphabet string (assigns each character an index).
+
+    Returns:
+        Sequence string representation of `one_hot`.
+
+    """
+    residue_idxs = np.argmax(one_hot, axis=1)
+    return "".join([alphabet[idx] for idx in residue_idxs])
+
+def differ_dict(wt, mutate):
+    d_dict = {}
+    mutate_str_list = []
+    if len(wt)!= len(mutate):
+        return "error"
+    for i in range(len(wt)):
+        if wt[i] != mutate[i]:
+            #print(str(i) + " " + str_1[i] + " " + str_2[i])
+            d_dict[i] = mutate[i]
+    if len(d_dict) > 0:
+        for v in d_dict.values():
+            mutate_str_list.append(v)
+    mutate_str = "".join(mutate_str_list)
+
+    return d_dict, mutate_str
+
+def map_to_full_len_gfp(start_seq, d_dict, truncated_str):
+    wt_list = list(start_seq)
+    for k, v in d_dict.items():
+        wt_list[v] = truncated_str[k]
+    gfp_mutate_seq = "".join(wt_list)
+    return gfp_mutate_seq
+
+def string_to_feature(string):
+    seq_list = []
+    seq_list.append(string)
+    seq_np = np.array(
+        [string_to_one_hot(seq, AAS) for seq in seq_list]
+    )
+    one_hots = torch.from_numpy(seq_np)
+    one_hots = one_hots.to(torch.float32)
+    return one_hots
+
+#def map_to_full_len_gfp():
+
+
+class Seq_env(object):
+    """sequence space for the env"""
+    def __init__(self,
+                 seq_len,
+                 alphabet,
+                 model,
+                 starting_seq,
+                 trust_radus,
+                 wt_seq,
+                 truncated,
+                 map_dict,
+                 ):
+        #自己加的变量
+        self.max_moves = trust_radus
+        self.move_count = 0
+        #自己加的变量
+        self.seq_len = seq_len#self.width = int(kwargs.get('width', 8))
+        self.vocab_size = len(alphabet)#self.height = int(kwargs.get('height', 8))
+
+        self.alphabet = alphabet
+        self.model = model
+        self.starting_seq = starting_seq
+        self.truncated = truncated
+        self.wt = wt_seq
+        self.map_dict = map_dict
+        ####改
+        self._state = string_to_one_hot(self.starting_seq, self.alphabet).astype(np.float32) #np.float32 是否需要改
+        #self.less_than_previous_count = []
+        self.unuseful_move = 0
+        self.states = {}
+        #self.episode_seqs = []
+        #model 的代码
+        self.model.eval()
+        self.availables = list(range(self.seq_len * self.vocab_size))
+        self.generated_seqs = []
+        self.generated_seqs.append(self.starting_seq)
+        self.from_truncated = wt_seq
+
+    def init_seq_state(self): #start_player=0
+        #self.episode_seqs = []
+        self.previous_fitness = -float("inf")
+        self.move_count = 0
+        #self.less_than_previous_count = []
+        self.unuseful_move = 0
+        #此处应该还会反复修改
+        #self._state = string_to_one_hot(self.starting_seq, self.alphabet).astype(np.float32)
+        #加入变异的起始状态
+        start = string_to_one_hot(self.starting_seq, self.alphabet).astype(np.float32)
+        self._state = start
+        # mut_len = np.random.poisson(2) + 1
+        # for i in range(mut_len):
+        #     pos = random.choice(list(range(start.shape[0])))
+        #     pre_res = np.where(start[pos]==1)
+        #     tmp_l = list(range(self.vocab_size))
+        #     tmp_l.remove(pre_res[0])
+        #     res = random.choice(tmp_l)
+        #     self._state[pos] = 0
+        #     self._state[pos, res] = 1
+        # 加入变异的起始状态
+        #   起始序列的_state_fitness
+        #str_1 = one_hot_to_string(self._state, AAS)
+        #self.episode_seqs.append(self.starting_seq)
+        # if self.truncated == True:
+        #     from_truncated = map_to_full_len_gfp(self.wt, self.map_dict)
+        model_input = string_to_one_hot(self.wt, self.alphabet).astype(np.float32)
+        # else:
+        #     model_input = start
+        # one_hots = torch.from_numpy(self._state)
+        one_hots = torch.from_numpy(model_input)
+        one_hots = one_hots.unsqueeze(0)
+        one_hots = one_hots.to(torch.float32)
+        # seq_dataset = MyDataset(one_hots, labels)
+        with torch.no_grad():
+            inputs = one_hots
+            inputs = inputs.permute(0, 2, 1)
+            # print('输入为：',inputs)
+            outputs = self.model(inputs)
+            outputs = outputs.squeeze()
+        if outputs:
+            self._state_fitness = outputs
+        #   起始序列的_state_fitness
+        # keep available moves in a list
+        # self.availables = list(range(self.seq_len * self.vocab_size))
+        self.states = {}
+        self.last_move = -1
+
+    def current_state(self):
+
+        square_state = np.zeros((self.seq_len, self.vocab_size))
+        square_state = self._state
+        return square_state.T
+    def do_mutate(self, move):
+        #
+        repeated_seq_ocurr = False
+        #
+        tmp_map_dict = {}
+        self.previous_fitness = self._state_fitness
+        self.move_count += 1
+        #self.availables.remove(move)
+        pos = move // self.vocab_size
+        res = move % self.vocab_size
+
+        if self._state[pos, res] == 1:
+            self.unuseful_move = 1
+            self._state_fitness = 0.0
+        else:
+            self._state[pos] = 0
+            self._state[pos, res] = 1
+
+            # if one_hot_to_string(self._state,AAS) in self.episode_seqs:
+            #     self.unuseful_move = 1
+            #     self._state_fitness = 0.0
+            # else:
+            #     self.episode_seqs.append(one_hot_to_string(self._state,AAS))
+            #previous fitness
+            if self.truncated == True:
+                truncated_str = one_hot_to_string(self._state,AAS)
+                # for ind, key in enumerate(self.map_dict):
+                #     tmp_map_dict[key] = truncated_str[ind]
+                self.from_truncated = map_to_full_len_gfp(self.wt, self.map_dict, truncated_str)
+                model_input = string_to_one_hot(self.from_truncated, self.alphabet).astype(np.float32)
+            else:
+                model_input = self._state
+
+            #one_hots_0 = self._state
+            #one_hots = torch.from_numpy(one_hots_0)
+            one_hots = torch.from_numpy(model_input)
+            one_hots = one_hots.unsqueeze(0)
+            one_hots = one_hots.to(torch.float32)
+            with torch.no_grad():
+                inputs = one_hots
+                inputs = inputs.permute(0, 2, 1)
+                # print('输入为：',inputs)
+                outputs = self.model(inputs)
+                outputs = outputs.squeeze()
+            if outputs:
+                self._state_fitness = outputs
+        #
+        current_seq = one_hot_to_string(self._state, AAS)
+        if current_seq in self.generated_seqs:
+            repeated_seq_ocurr = True
+            #self._state_fitness = 0.0
+            self.availables.remove(move)
+        else:
+            self.generated_seqs.append(current_seq)
+        if self._state_fitness < self.previous_fitness and not repeated_seq_ocurr:  # 0.6* 0.75*
+            #self._state_fitness = 0.0
+            self.availables.remove(move)
+        self.last_move = move
+        #
+
+
+
+    def mutation_end(self):
+
+        if self.move_count >= self.max_moves: # > 和 >= 的区别
+            return True
+        if self.unuseful_move == 1:
+            return True
+        if self._state_fitness < self.previous_fitness:  # 0.6* 0.75*
+            return True
+
+        return False
+
+class Mutate(object):
+    """mutating server"""
+
+    def __init__(self, Seq_env, **kwargs):
+        self.Seq_env = Seq_env
+
+    def start_p_mutating(self, player1, player2, start_player=0, is_shown=1):
+        """start a game between two players"""
+        if start_player not in (0, 1):
+            raise Exception('start_player should be either 0 (player1 first) '
+                            'or 1 (player2 first)')
+        self.Seq_env.init_board(start_player)
+        p1, p2 = self.Seq_env.players
+        player1.set_player_ind(p1)
+        player2.set_player_ind(p2)
+        players = {p1: player1, p2: player2}
+        if is_shown:
+            self.graphic(self.Seq_env, player1.player, player2.player)
+        while True:
+            current_player = self.Seq_env.get_current_player()
+            player_in_turn = players[current_player]
+            move = player_in_turn.get_action(self.Seq_env)
+            self.Seq_env.do_move(move)
+            if is_shown:
+                self.graphic(self.Seq_env, player1.player, player2.player)
+            end, winner = self.Seq_env.game_end()
+            if end:
+                if is_shown:
+                    if winner != -1:
+                        print("Game end. Winner is", players[winner])
+                    else:
+                        print("Game end. Tie")
+                return winner
+
+    def start_mutating(self, mutater, is_shown=0, temp=1e-3):#mutater,
+        """ start mutating using a MCTS player, reuse the search tree,
+        and store the self-play data: (state, mcts_probs, z) for training
+        """
+        self.Seq_env.init_seq_state()
+        generated_seqs, seq_fitness = [], []
+        #p1, p2 = self.board.players
+        states, mcts_probs, reward_z = [], [], [] #, current_players #, []
+        while True:
+            move, move_probs = mutater.get_action(self.Seq_env,
+                                                 temp=temp,
+                                                 return_prob=1)
+            if move:
+                # store the data
+                states.append(self.Seq_env.current_state())
+                mcts_probs.append(move_probs)
+                reward_z.append(self.Seq_env._state_fitness)
+                #
+                #
+                # truncated_str = one_hot_to_string(self.Seq_env._state, AAS)
+                # full_len_seq = map_to_full_len_gfp(self.wt, self.map_dict, truncated_str)
+                generated_seqs.append(self.Seq_env.from_truncated)
+                seq_fitness.append(float(self.Seq_env._state_fitness))
+                #
+                # perform a move
+                self.Seq_env.do_mutate(move)
+                print("move_fitness: %f\n" % (self.Seq_env._state_fitness))
+                state_string = one_hot_to_string(self.Seq_env._state, AAS)
+                print(state_string)
+            end = self.Seq_env.mutation_end()
+            if end:
+
+                #
+                generated_seqs.append(self.Seq_env.from_truncated)
+                seq_fitness.append(float(self.Seq_env._state_fitness))
+                #
+                mutater.reset_Mutater()
+                if is_shown:
+
+                    print("Mutation end.")
+                return zip(states, mcts_probs, reward_z), zip(generated_seqs, seq_fitness)  #winner,
---
+++ code/PAB1_GFP_task/evoplay_custom_design_with_preselected_sites/train_m.py
@@ -0,0 +1,333 @@
+# -*- coding: utf-8 -*-
+"""
+An implementation of the training pipeline of EvoPlay for PAB1/GFP protein mutation
+
+@author: Yi Wang
+"""
+
+from __future__ import print_function
+import random
+import numpy as np
+import pandas as pd
+from collections import defaultdict, deque
+from sequence_env_differ import Seq_env, Mutate
+#from sequence_env import Seq_env, Mutate
+from mcts_pure import MCTSPlayer as MCTS_Pure
+from mcts_alphaZero_mutate import MCTSMutater
+#from p_v_net_torch import PolicyValueNet  # Pytorch
+from p_v_net_2 import PolicyValueNet
+from env_model import CNN
+import torch
+from typing import List, Union
+
+
+#pre_select genes
+p_s_fr = open(r"E:\project_s\MCTS_Mutate_2_preSelect\GFP_wt_HighLikelihood_variant.txt")
+ll = p_s_fr.readlines()
+set1 = set()
+for i in range(4,405):
+    tm = ll[i].split("\t")[0][1:-1].split(", ")[0]
+    set1.add(tm)
+preSelect_list = list(set1)
+preSelect_list = [int(tm) for tm in preSelect_list]
+preSelect_list_1 = sorted(preSelect_list)
+#pre_select genes
+
+#要换成238
+gfp_wt_sequence = "MSKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTLSYGVQCFSRYPDHMKQHDFFKSAMPEGYVQERTIFFKDDGNYKTRAEVKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYNSHNVYIMADKQKNGIKVNFKIRHNIEDGSVQLADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNEKRDHMVLLEFVTAAGITHGMDELYK"
+starts = {
+        "ed_10_wt": "MSKGEVLFTGVVPILVEMDGDVNGHKFSVSGEGEGDATYGKLTTKFTCTTGKLPVPWPTKVTTLSYRVQCFSRYPDVMKQHDFFKSAMPEGYVQERTIFFKDDGNYKTRAEVQFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYNSHNVYIMADKQKNGIKVNFKIRHNIEDGSVQLADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNIKRDCMVLLEFVTAAGITHGMDELYK",  # noqa: E501
+        "ed_18_wt": "MSKGEHLFTGVVPILVELDGDVNGKKFSVSGEGQGDATYGKLTLKFICTTAKVHVPWCTLVTTLSYGVQCFSRYPDHMKQHDFFKGAMPEGYVQERTIFFKDIGNYKLRAEVKFEGDTLVNRIELKGIDFKEDGNIHGHKLEYNYNSQNVYIMASKQKNGIKVNFKIRLNIEDGSVQLAEHYQVNTPIGDFPVLLPDNHKLSAQSADSKDPNEKRDHMHLLEFVTAVGITHGMDELYK",  # noqa: E501
+        "ed_31_wt": "MSKGEELFSGVQPILVELDGCVNGHKFSVSGEGEIDATYGKLTLKFICTTWKLPMPWPCLVTFGSYGVQCFSRYRDHPKQHDFFKSAVPEGYVQERTIFMKDDLLYKTRAEVKFEGLTLVNRIELKGKDFKEDGNILGHKLEYNYNSHCVYPMADWNKNWIKVNSKIRLPIEDGSVILADHYQQNTPIGDQPVLLPENHYLSTQSALSKDPEEKGDLMVLLEFVTAAGITHGMDELYK",  # noqa: E501
+        "0.1300": "MSKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTLSYGVQCFSRYPDHMKQHDFFKSAIPEGYVQERTIFFKDDGNYKTRAEVKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYNSHNVYIMADKQKNGIKVNFKIRHNIEDGSVQLADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNEKRVHMVLLEFVTAAGTTHGMDEQYK" ,
+        "0.005": "MSKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTLSYGVQCFSRYPDHMKQHDFFKSAIPEGYVQERTIFFKDDGNYKTRAEVKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYNSHNVYIMAVKQKDGIKVNFKNRHNIEDGSVRLADRYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNEKRDHMVLLEFVTAAGITHGMDELYK" ,
+        "0.59": "MSKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTLSYGVQCFSRYPDHMKQHDFFKSAMPEGYVQERTIFFKDDGNYKTRSEVKVEGDTLVNRIELKGIDLKEDGNILGHKLEYNYNSHNVYIMADKQKNGIKVNFKIRHNIEDGSVQLADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPSEKRDHMVLLEFVTAAGITHGMDELYK",
+        "0.007": "MSKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTLSYGVQCFSRYPDHMKQHDFFKSAMPEGYVQERTFFFKDDGNYKTRAEAKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYNSHNVYIMADKQKNGIKVNFKIRHNIEDGSVQLADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNEKRDHMVLLEFVTAAGITHGMDELYK",
+    }
+AAS = "ILVAGMFYWEDQNHCRKSTP"
+
+def string_to_one_hot(sequence: str, alphabet: str) -> np.ndarray:
+
+    out = np.zeros((len(sequence), len(alphabet)))
+    for i in range(len(sequence)):
+        out[i, alphabet.index(sequence[i])] = 1
+    return out
+
+def one_hot_to_string(
+    one_hot: Union[List[List[int]], np.ndarray], alphabet: str
+) -> str:
+    """
+    Return the sequence string representing a one-hot vector according to an alphabet.
+
+    Args:
+        one_hot: One-hot of shape `(len(sequence), len(alphabet)` representing
+            a sequence.
+        alphabet: Alphabet string (assigns each character an index).
+
+    Returns:
+        Sequence string representation of `one_hot`.
+
+    """
+    residue_idxs = np.argmax(one_hot, axis=1)
+    return "".join([alphabet[idx] for idx in residue_idxs])
+
+def differ_dict(wt, mutate):
+    d_dict = {}
+    mutate_str_list = []
+    if len(wt)!= len(mutate):
+        return "error"
+    for i in range(len(wt)):
+        if wt[i] != mutate[i]:
+            #print(str(i) + " " + str_1[i] + " " + str_2[i])
+            d_dict[i] = mutate[i]
+    if len(d_dict) > 0:
+        for v in d_dict.values():
+            mutate_str_list.append(v)
+    mutate_str = "".join(mutate_str_list)
+
+    return d_dict, mutate_str
+
+def pre_select_dict(start_seq, pre_select_list):
+    d_dict = {}
+    mutate_str_list = []
+    for i in range(len(pre_select_list)):
+        d_dict[i] = pre_select_list[i]
+
+
+    for ind in pre_select_list:
+        mutate_str_list.append(start_seq[ind])
+    # if len(d_dict) > 0:
+    #     for v in d_dict.values():
+    #         mutate_str_list.append(v)
+    mutate_str = "".join(mutate_str_list)
+    return d_dict, mutate_str
+
+class TrainPipeline():
+    def __init__(self, start_seq, alphabet, model, wt_seq, pre_select_list, trust_radius, preSelect=False, init_model=None): #init_model=None
+        # params of the board and the game
+        if preSelect == True:
+            d_dict, mutate_str = pre_select_dict(start_seq, pre_select_list)
+            self.starting_seq = mutate_str
+            truncated = True
+        else:
+            self.starting_seq = start_seq
+            truncated = False
+        # if len(d_dict) >= 6:
+        #     self.starting_seq = mutate_str
+        #     truncated = True
+        # else:
+        #     self.starting_seq = start_seq
+        #     truncated = False
+        self.seq_len = len(self.starting_seq)
+        self.vocab_size = len(alphabet)
+        self.n_in_row = 4
+        self.seq_env = Seq_env(
+            self.seq_len,
+            alphabet,
+            model,
+            self.starting_seq,
+            trust_radius,
+            start_seq,
+            truncated,
+            d_dict,
+        )  #n_in_row=self.n_in_row
+        self.mutate = Mutate(self.seq_env)
+        # training params
+        self.learn_rate = 2e-3
+        self.lr_multiplier = 1.0  # adaptively adjust the learning rate based on KL
+        self.temp = 1.0  # the temperature param
+        self.n_playout = 400  # num of simulations for each move 400, 800
+        self.c_puct = 5
+        self.buffer_size = 10000
+        self.batch_size = 8  # mini-batch size for training  512
+        self.data_buffer = deque(maxlen=self.buffer_size)
+        self.play_batch_size = 1
+        self.epochs = 5  # num of train_steps for each update
+        self.kl_targ = 0.02
+        self.check_freq = 50
+        self.game_batch_num = 1500
+        self.best_win_ratio = 0.0
+        # num of simulations used for the pure mcts, which is used as
+        # the opponent to evaluate the trained policy
+        self.pure_mcts_playout_num = 1000
+        #self_added
+        self.buffer_no_extend = False
+        #self_added
+        #GFP
+        self.seqs_and_fitness = []
+        #GFP
+        if init_model:
+            # start training from an initial policy-value net
+            self.policy_value_net = PolicyValueNet(self.seq_len,
+                                                   self.vocab_size,
+                                                   model_file=init_model)
+        else:
+            # start training from a new policy-value net
+            self.policy_value_net = PolicyValueNet(self.seq_len,
+                                                   self.vocab_size)
+        self.mcts_player = MCTSMutater(self.policy_value_net.policy_value_fn,
+                                      c_puct=self.c_puct,
+                                      n_playout=self.n_playout,
+                                      is_selfplay=1)
+
+    def collect_selfplay_data(self, n_games=1):
+        """collect self-play data for training"""
+        self.buffer_no_extend = False
+        for i in range(n_games):
+            play_data, seqs_and_fitness = self.mutate.start_mutating(self.mcts_player,
+                                                          temp=self.temp)    #winner,
+            play_data = list(play_data)[:]
+            self.episode_len = len(play_data)
+            if self.episode_len == 0:
+                self.buffer_no_extend = True
+            # augment the data
+            #play_data = self.get_equi_data(play_data)
+            else:
+                self.data_buffer.extend(play_data)
+                s_f_list = list(seqs_and_fitness)
+                for seq, fitness in s_f_list[1:]:
+                 self.seqs_and_fitness.append({"sequence": seq, "fitness":fitness})
+
+    def policy_update(self):
+        """update the policy-value net"""
+        mini_batch = random.sample(self.data_buffer, self.batch_size)
+        state_batch = [data[0] for data in mini_batch]
+        mcts_probs_batch = [data[1] for data in mini_batch]
+        winner_batch = [data[2] for data in mini_batch]
+        old_probs, old_v = self.policy_value_net.policy_value(state_batch)
+        for i in range(self.epochs):
+            loss, entropy = self.policy_value_net.train_step(
+                    state_batch,
+                    mcts_probs_batch,
+                    winner_batch,
+                    self.learn_rate*self.lr_multiplier)
+            new_probs, new_v = self.policy_value_net.policy_value(state_batch)
+            kl = np.mean(np.sum(old_probs * (
+                    np.log(old_probs + 1e-10) - np.log(new_probs + 1e-10)),
+                    axis=1)
+            )
+            if kl > self.kl_targ * 4:  # early stopping if D_KL diverges badly
+                break
+        # adaptively adjust the learning rate
+        if kl > self.kl_targ * 2 and self.lr_multiplier > 0.1:
+            self.lr_multiplier /= 1.5
+        elif kl < self.kl_targ / 2 and self.lr_multiplier < 10:
+            self.lr_multiplier *= 1.5
+
+        explained_var_old = (1 -
+                             np.var(np.array(winner_batch) - old_v.flatten()) /
+                             np.var(np.array(winner_batch)))
+        explained_var_new = (1 -
+                             np.var(np.array(winner_batch) - new_v.flatten()) /
+                             np.var(np.array(winner_batch)))
+        print(("kl:{:.5f},"
+               "lr_multiplier:{:.3f},"
+               "loss:{},"
+               "entropy:{},"
+               "explained_var_old:{:.3f},"
+               "explained_var_new:{:.3f}"
+               ).format(kl,
+                        self.lr_multiplier,
+                        loss,
+                        entropy,
+                        explained_var_old,
+                        explained_var_new))
+        return loss, entropy
+
+    def policy_evaluate(self, n_games=10):
+
+        current_mcts_player = MCTSMutater(self.policy_value_net.policy_value_fn,
+                                         c_puct=self.c_puct,
+                                         n_playout=self.n_playout)
+        pure_mcts_player = MCTS_Pure(c_puct=5,
+                                     n_playout=self.pure_mcts_playout_num)
+        win_cnt = defaultdict(int)
+        for i in range(n_games):   #这个地方可能需要重新考虑一下
+            winner = self.mutate.start_p_mutating(current_mcts_player,
+                                          pure_mcts_player,
+                                          start_player=i % 2,
+                                          is_shown=0)
+            win_cnt[winner] += 1
+        win_ratio = 1.0*(win_cnt[1] + 0.5*win_cnt[-1]) / n_games
+        print("num_playouts:{}, win: {}, lose: {}, tie:{}".format(
+                self.pure_mcts_playout_num,
+                win_cnt[1], win_cnt[2], win_cnt[-1]))
+        return win_ratio
+
+    def run(self):
+        """run the training pipeline"""
+        saved_flag_1 = 0
+        saved_flag_2 = 0
+        saved_flag_3 = 0
+        saved_flag_4 = 0
+        try:
+            for i in range(self.game_batch_num):
+                self.collect_selfplay_data(self.play_batch_size)
+                print("batch i:{}, episode_len:{}".format(
+                        i+1, self.episode_len))
+                # saving result reqs
+                if len(self.seqs_and_fitness) >= 50 and saved_flag_1 == 0:
+                    saved_flag_1 = 1
+                    # and saved_flag != 1: #100
+                    df = pd.DataFrame(self.seqs_and_fitness)
+                    df.to_csv(r"./result_seqs/MCTS_generated_gfp_selected_50.csv", index=False)
+                    print("saving seqs.............")
+                if len(self.seqs_and_fitness) >= 100 and saved_flag_2 == 0:
+                    saved_flag_2 = 1
+                    # and saved_flag != 1: #100
+                    df = pd.DataFrame(self.seqs_and_fitness)
+                    df.to_csv(r"./result_seqs/MCTS_generated_gfp_selected_100.csv", index=False)
+                    print("saving seqs.............")
+                if len(self.seqs_and_fitness) >= 150 and saved_flag_3 == 0:
+                    saved_flag_3 = 1
+                    # and saved_flag != 1: #100
+                    df = pd.DataFrame(self.seqs_and_fitness)
+                    df.to_csv(r"./result_seqs/MCTS_generated_gfp_selected_150.csv", index=False)
+                    print("saving seqs.............")
+                if len(self.seqs_and_fitness) >= 200 and saved_flag_4 == 0:
+                    saved_flag_4 = 1
+                    # and saved_flag != 1: #100
+                    df = pd.DataFrame(self.seqs_and_fitness)
+                    df.to_csv(r"./result_seqs/MCTS_generated_gfp_selected_200.csv", index=False)
+                    print("saving seqs.............")
+                # saving result reqs
+                if len(self.data_buffer) > self.batch_size and self.buffer_no_extend == False:
+                    loss, entropy = self.policy_update()
+                # check the performance of the current model,
+                # and save the model params
+                # if (i+1) % self.check_freq == 0:
+                #     print("current self-play batch: {}".format(i+1))
+                #     win_ratio = self.policy_evaluate()
+                #     self.policy_value_net.save_model('./current_policy.model')
+                #     if win_ratio > self.best_win_ratio:
+                #         print("New best policy!!!!!!!!")
+                #         self.best_win_ratio = win_ratio
+                #         # update the best_policy
+                #         self.policy_value_net.save_model('./best_policy.model')
+                #         if (self.best_win_ratio == 1.0 and
+                #                 self.pure_mcts_playout_num < 5000):
+                #             self.pure_mcts_playout_num += 1000
+                #             self.best_win_ratio = 0.0
+        except KeyboardInterrupt:
+            print('\n\rquit')
+
+#model predict
+
+if __name__ == '__main__':
+    MODEL_PATH = './gfp_checkpoint/CNN_GFP_checkpoint_19_0.0073_0.0519.tar'
+    model2 = CNN(
+        len(gfp_wt_sequence),
+        len(AAS),
+
+    )
+    model2.load_state_dict(torch.load(MODEL_PATH))
+    training_pipeline = TrainPipeline(
+        starts["0.1300"], # starts["ed_10_wt"] ,gfp_wt_sequence, 0.1300  ,0.59, 0.005 ,0.007
+        AAS,
+        model2,
+        gfp_wt_sequence,
+        preSelect_list_1,
+        trust_radius=15,
+        preSelect=True,
+    )
+    training_pipeline.run()
